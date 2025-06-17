3b403dd6c8455f3d6e917cc5fba0a385be688e31
federicowoodward <woodfederico@gmail.com>
2025-06-17 17:28:21 -0300

    mejoro codigo en dashboard

--- obras-front/src/app/pages/work/work.ts
+++ obras-front/src/app/pages/work/work.ts
@@ -1,18 +1,26 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-work',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './work.html',
+  styleUrls: ['./work.scss'],
 })
 export class Work {
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
   id: string | null = null;
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log('ID:', this.id);
+  }
+
+  goBack() {
+    this.router.navigate(['/works']);
   }
 }
