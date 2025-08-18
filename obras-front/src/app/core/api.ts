ee9c302902def4c5ce43ca4b1d8619989a7b006c
federicowoodward <woodfederico@gmail.com>
2025-08-17 23:55:32 -0300

    fix build angular

--- obras-front/src/app/core/api.ts
+++ obras-front/src/app/core/api.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { environment } from '../environment/environment';
+import { environment } from '../../environment/environment';
 
 type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
